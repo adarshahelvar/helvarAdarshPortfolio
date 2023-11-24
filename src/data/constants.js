830562e21beaabc26130e8db5035d97fa2b12a56
Adarsha A <108178521+adarshahelvar@users.noreply.github.com>
2023-11-24 16:19:39 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/adarshahelvar",
   resume:
-    "https://drive.google.com/file/d/1yLkxyYH6nDdqCPXimnqYopCzfKidLHqJ/view?usp=sharing",
+    "https://drive.google.com/file/d/1_GQc9VcnvXXrVIqXyv55NjrsInzYmWhu/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/adarshahelvar/",
   twitter: "https://twitter.com/adarsha_helvar",
   insta: "https://www.instagram.com/adarsh_helvar/",
